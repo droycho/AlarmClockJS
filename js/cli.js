da119d95f85b082b59caa56dd80b02c7c6ee57e0
DJ Roychoudhury and LaTaevia Berry <dj@>
2016-05-24 14:37:11 -0700

    add edits

---
+++ js/cli.js
@@ -0,0 +1,13 @@
+///only for command line
+
+
+// var pingPong = require('./alarm-clock.js').pingPong;
+// var prompt = require('prompt');
+// prompt.start();
+//
+// prompt.get('goal', function(err, result) {
+//   var result = pingPong(result.goal);
+//   result.forEach(function(element) {
+//     console.log(element);
+//   });
+// });
